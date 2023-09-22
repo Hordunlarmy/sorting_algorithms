973d542955c35237287d4c640d1c6712c41ba9c9
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-22 20:15:09 +0100

    Add selection sort implementation and test in main

--- sort.h
+++ sort.h
@@ -9,5 +9,6 @@
 /* Prototypes Of Functions Used */
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
