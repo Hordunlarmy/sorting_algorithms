bb1a7c71804943aac5934794c621376b1f1d0842
Hordunlarmy <Hordunlarmy@gmail.com>
2023-09-22 17:52:10 +0100

    Add bubble sort algorithm implementation and print array function

---
+++ print_array.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: size of the array
+ * Return: Nothing
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* Standard Library Functions */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Prototypes Of Functions Used */
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* SORT_H */
